f4b9f85cfdb687178136de87c1e4618b691d0f3a
Sonia Choudhury <sonia.choudhury@hotmail.co.uk>
2021-05-23 21:40:45 +0100

    logic

--- vue-weather/src/App.vue
+++ vue-weather/src/App.vue
@@ -2,17 +2,24 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search...">
+        <input type="text" 
+        class="search-bar" 
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather"
+        >
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
-          <div class="date">Monday 20th January 2020</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country}}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
+
         <div class="weather-box">
-          <div class="temperature">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temperature"> {{ weather.main.temp }}°c</div>
+          <div class="img"><img v-bind:src="`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`"></div>
+          <div class="weather"> {{ weather.weather[0].main }} </div>
         </div>
       </div>
 
@@ -27,10 +34,37 @@ export default {
   name: 'App',
   data() {
     return {
-      api_key: '71a044cb9da982f291b5ccd1de19279d'
+      api_key: '71a044cb9da982f291b5ccd1de19279d',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+       if (e.key === "Enter") {
+         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res=> { 
+            return res.json()
+            })
+          .then(this.setResults);
+          console.log(this.setResults)
+       } 
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
-
 }
 </script>
 
@@ -84,6 +118,51 @@ main {
   background-color: rgba(255, 255, 255, 0.75);
   box-shadow: 0px 0px 16px rgba(255, 255, 255, 0.25);
   border-radius: 16px 0px 16px 0px;
+
+}
+
+.location-box .location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0, 0.25)
+}
+
+ .location-box .date {
+  color: white;
+  font-size: 20px;
+  font-weight: 500;
+  font-style: italic;
+  text-align: center;
+ }
+
+ .weather-box {
+   text-align: center;
+ }
+
+ .weather-box .temperature {
+   padding: 25px;
+   color: white;
+   font-size: 102px;
+   font-weight: 900;
+   text-shadow: 3px 6px rgba(0,0,0, 0.25);
+   background-color: rgba(255, 255, 255, 0.25);
+   border-radius: 16px;
+   margin: 30px 0px;
+ }
+
+.weather-box .weather {
+   color: white;
+   font-size: 48px;
+   font-weight: 700;
+   /* font-style: italic; */
+   text-shadow: 3px 6px rgba(0,0,0, 0.25);
+
+}
+.temperature {
+  display: flex;
+  flex-direction: column;
 
 }
 </style>
